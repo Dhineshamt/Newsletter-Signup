305f94f37024529920d9d6950ed0b5760d6f10f0
Dhineshamt <a.dhinesh1@gmail.com>
2019-12-25 16:04:15 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+})
+
+app.post('/', (req, res)=>{
+    let fName = req.body.fName;
+    let lName = req.body.lName;
+    let email = req.body.email;
+
+    let data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fName,
+                LNAME: lName
+            }
+        }]
+    }
+
+    let jsonData = JSON.stringify(data);
+
+    let options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/6f5878fbcf",
+        method: "POST",
+        headers: {
+            "Authorization": "dhinesh 25c562ad50eec09a71799d2696e5dfec-us4",
+        },
+        body: jsonData
+    }
+
+    request(options, (error, response, body) => {
+        if(error) {
+            res.sendFile(__dirname + '/failure.html');
+        } else if(response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else res.sendFile(__dirname + '/failure.html');  
+
+    });
+})
+
+app.post('/failure', (req,res) => {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server is running in port: 3000");
+});
